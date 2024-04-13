7ee21e416102e80994ae4bf8b9d2bf0248b943fc
Laurence <laurence.jones@live.co.uk>
2024-04-13 16:46:01 +0100

    wip: implement basic spoa with current example on spoa docs

--- go.mod
+++ go.mod
@@ -36,6 +36,7 @@ require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
+	github.com/negasus/haproxy-spoe-go v1.0.5 // indirect
 	github.com/oklog/ulid v1.3.1 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/prometheus/client_model v0.5.0 // indirect
---
+++ pkg/spoa/root.go
@@ -0,0 +1,123 @@
+package spoa
+
+import (
+	"fmt"
+	"math/rand"
+	"net"
+	"os"
+	"syscall"
+
+	"github.com/crowdsecurity/crowdsec-spoa/pkg/cfg"
+	"github.com/negasus/haproxy-spoe-go/action"
+	"github.com/negasus/haproxy-spoe-go/agent"
+	"github.com/negasus/haproxy-spoe-go/request"
+	log "github.com/sirupsen/logrus"
+)
+
+type Spoa struct {
+	ListenAddr   net.Listener
+	ListenSocket net.Listener
+	Server       *agent.Agent
+}
+
+func New(cfg *cfg.BouncerConfig) (*Spoa, error) {
+	s := &Spoa{}
+
+	if cfg.ListenAddr != "" {
+		addr, err := net.Listen("tcp", cfg.ListenAddr)
+		if err != nil {
+			return nil, fmt.Errorf("failed to listen on %s: %v", cfg.ListenAddr, err)
+		}
+		s.ListenAddr = addr
+	}
+
+	if cfg.ListenSocket != "" {
+		// Get current gid
+		gid := os.Getgid()
+		// Get existing socket stat
+		fileInfo, err := os.Stat(cfg.ListenSocket)
+		if err != nil {
+			if !os.IsNotExist(err) {
+				return nil, fmt.Errorf("failed to stat socket %s: %v", cfg.ListenSocket, err)
+			}
+			stat, ok := fileInfo.Sys().(*syscall.Stat_t)
+			if !ok {
+				return nil, fmt.Errorf("failed to get socket stat")
+			}
+			// Set gid to existing socket
+			gid = int(stat.Gid)
+		}
+
+		if err := os.Remove(cfg.ListenSocket); err != nil {
+			if !os.IsNotExist(err) {
+				return nil, fmt.Errorf("failed to remove socket %s: %v", cfg.ListenSocket, err)
+			}
+		}
+
+		origUmask := syscall.Umask(0o777)
+
+		addr, err := net.Listen("unix", cfg.ListenSocket)
+		if err != nil {
+			return nil, fmt.Errorf("failed to listen on %s: %v", cfg.ListenSocket, err)
+		}
+
+		syscall.Umask(origUmask)
+
+		os.Chown(cfg.ListenSocket, 0, gid)
+		os.Chmod(cfg.ListenSocket, 0o660)
+
+		s.ListenSocket = addr
+	}
+
+	s.Server = agent.New(handler, log.StandardLogger())
+
+	return s, nil
+}
+
+func (s *Spoa) ServeTCP() error {
+	if s.ListenAddr == nil {
+		return nil
+	}
+	log.Infof("Serving TCP server on %s", s.ListenAddr.Addr().String())
+	return s.Server.Serve(s.ListenAddr)
+}
+
+func (s *Spoa) ServeUnix() error {
+	if s.ListenSocket == nil {
+		return nil
+	}
+
+	log.Infof("Serving Unix server on %s", s.ListenSocket.Addr().String())
+	return s.Server.Serve(s.ListenSocket)
+}
+
+func handler(req *request.Request) {
+
+	log.Printf("handle request EngineID: '%s', StreamID: '%d', FrameID: '%d' with %d messages\n", req.EngineID, req.StreamID, req.FrameID, req.Messages.Len())
+
+	messageName := "get-ip-reputation"
+
+	mes, err := req.Messages.GetByName(messageName)
+	if err != nil {
+		log.Printf("message %s not found: %v", messageName, err)
+		return
+	}
+
+	ipValue, ok := mes.KV.Get("ip")
+	if !ok {
+		log.Printf("var 'ip' not found in message")
+		return
+	}
+
+	ip, ok := ipValue.(net.IP)
+	if !ok {
+		log.Printf("var 'ip' has wrong type. expect IP addr")
+		return
+	}
+
+	ipScore := rand.Intn(100)
+
+	log.Printf("IP: %s, send score '%d'", ip.String(), ipScore)
+
+	req.Actions.SetVar(action.ScopeSession, "ip_score", ipScore)
+}
