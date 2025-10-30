2e702a4d0a39d7ffba4c4a24029f3823b409f7bc
Laurence <laurence.jones@live.co.uk>
2025-10-30 15:38:22 +0000

    test(worker): make TCPAddr assertion safe in tests

--- internal/worker/manager_test.go
+++ internal/worker/manager_test.go
@@ -21,9 +21,9 @@ func getFreePort(t *testing.T) string {
 	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	require.NoError(t, err)
 	defer listener.Close()
-    tcpAddr, ok := listener.Addr().(*net.TCPAddr)
-    require.True(t, ok, "listener.Addr should be *net.TCPAddr")
-    return fmt.Sprintf("127.0.0.1:%d", tcpAddr.Port)
+	tcpAddr, ok := listener.Addr().(*net.TCPAddr)
+	require.True(t, ok, "listener.Addr should be *net.TCPAddr")
+	return fmt.Sprintf("127.0.0.1:%d", tcpAddr.Port)
 }
 
 func TestNewManager(t *testing.T) {
